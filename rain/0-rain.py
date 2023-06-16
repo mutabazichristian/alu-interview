bb0436826790da8fdfd2a9ad9e35ba44a1e114b6
mutabazichristian <c.mutabazi@alustudent.com>
2023-06-16 23:49:52 +0200

    tasl

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,16 +1,39 @@
 #!/usr/bin/python3
 """
-Documentation
+minOperations module
+
+This module provides a method to calculate the fewest
+number of operations required to obtain a desired count
+of H characters in a file, given the operations Copy All and Paste.
+
 """
-def rain(walls):
-    """walls"""
-    if len(walls) == 0:
+import math
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations required to
+    obtain exactly n H characters in the file.
+
+    Parameters:
+        n (int): The desired count of H characters.
+
+    Returns:
+        int: The minimum number of operations required.
+        If n is impossible to achieve or invalid(negative), returns 0.
+
+    """
+    if n <= 0:
         return 0
-    for i in range(1, len(walls)):
-        """loops"""
-        if (walls[i] >= walls[i-1]):
-            amount += walls[i-1]
-        else:
-            amount += walls[1]
-        i+=1
-    return amount
+
+    operations = 0
+
+    for i in range(2, int(math.sqrt(n)) + 1):
+        while n % i == 0:
+            operations += i
+            n = n // i
+
+    if n > 1:
+        operations += n
+
+    return operations
